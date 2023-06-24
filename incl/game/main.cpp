9ebb52085369f14e64ce9933d05cb637f0fd8114
tuxedcat <tuxedcat@gmail.com>
2023-06-25 04:59:12 +0900

    viewport and projection

--- incl/game/main.cpp
+++ incl/game/main.cpp
@@ -3,13 +3,43 @@
 
 using namespace std;
 
+void drawSphere(int slices, int stacks){
+	for (int i = 0; i < slices; ++i){
+		double lat0 = PI * (-0.5 + static_cast<double>(i) / slices);
+		double lat1 = PI * (-0.5 + static_cast<double>(i + 1) / slices);
+		double y0 = sin(lat0);
+		double y1 = sin(lat1);
+		double cosLat0 = cos(lat0);
+		double cosLat1 = cos(lat1);
+		
+		glBegin(GL_QUAD_STRIP);
+		for (int j = 0; j <= stacks; ++j){
+			double lng = 2 * PI * static_cast<double>(j) / stacks;
+			double x = cos(lng);
+			double z = sin(lng);
+			
+			glNormal3f(x * cosLat0, y0, z * cosLat0);
+			glTexCoord2f(static_cast<double>(j) / stacks, static_cast<double>(i) / slices);
+			glVertex3f(x * cosLat0, y0, z * cosLat0);
+			
+			glNormal3f(x * cosLat1, y1, z * cosLat1);
+			glTexCoord2f(static_cast<double>(j) / stacks, static_cast<double>(i + 1) / slices);
+			glVertex3f(x * cosLat1, y1, z * cosLat1);
+		}
+		glEnd();
+	}
+}
+
 void display(){
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
+	glTranslatef(0,0,-10);
+	drawSphere(8, 8);
 }
 
 int main(){
-	if(!glfwInit()) {
+	if(!glfwInit()){
 		return -1;
 	}
 
@@ -19,34 +49,24 @@ int main(){
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+	glClearColor(0.6, 0.85, 1.0, 0.0);
+
+	while(!glfwWindowShouldClose(window)){
+		// Set viewport
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
 		glViewport(0, 0, width, height);
 
+		// Set projection
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		gluPerspective(60.0, (GLfloat)width / (GLfloat)height, 0.1, 100.0);
+		gluPerspective(60.0, static_cast<double>(width) / static_cast<double>(height), 0.1, 100.0);
 
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-	});
-
-	GLfloat mat_specular[] = {1.0, 1.0, 1.0, 1.0};
-	GLfloat mat_shininess[] = {50.0};
-	glClearColor(0.6, 0.85, 1.0, 0.0);
-	glShadeModel(GL_SMOOTH);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_DEPTH_TEST);
-
-	while (!glfwWindowShouldClose(window)){
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		display();
 		glfwSwapBuffers(window);
+
 		glfwPollEvents();
 	}
-	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
