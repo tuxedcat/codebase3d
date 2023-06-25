3b7c04af7400dfe871cea75526403fa9a80e02c0
tuxedcat <tuxedcat@gmail.com>
2023-06-25 11:37:55 +0900

    build update

--- tool/build.sh
+++ tool/build.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 set -e
 mkdir -p build
-src=${1:-"src/main.cpp"}
-build_type=${2:-"Debug"}
+build_type=${1:-"Debug"}
 
 ############################
 #   build execution file   #
 ############################
-mkdir -p build/$src/$build_type
-cmake -Bbuild/$src/$build_type -DSOURCE_PATH=${src} -DCMAKE_BUILD_TYPE=${build_type} -GNinja
-cmake --build build/$src/$build_type 
+mkdir -p build/$build_type
+cmake -Bbuild/$build_type -DCMAKE_BUILD_TYPE=${build_type} -GNinja
+cmake --build build/$build_type 
